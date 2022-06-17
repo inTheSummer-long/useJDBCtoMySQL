6c00764a010a378f6de980ce177e21c51ba97cd9
long <2043602397@qq.com>
2022-06-17 13:51:55 +0800

    second

---
+++ fruit.sql
@@ -0,0 +1,26 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 8.0.28 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `t_fruit` (
+	`fid` int ,
+	`fname` varchar ,
+	`price` int ,
+	`fcount` int ,
+	`remark` varchar 
+); 
+insert into `t_fruit` (`fid`, `fname`, `price`, `fcount`, `remark`) values('37','李子','21','80','好吃，不贵');
+insert into `t_fruit` (`fid`, `fname`, `price`, `fcount`, `remark`) values('38','芭蕉','23','12','美滴很');
+insert into `t_fruit` (`fid`, `fname`, `price`, `fcount`, `remark`) values('39','水蜜桃','23','12','是水蜜桃不是杀马特');
+insert into `t_fruit` (`fid`, `fname`, `price`, `fcount`, `remark`) values('40','火龙果','22','80','美滴很');
+insert into `t_fruit` (`fid`, `fname`, `price`, `fcount`, `remark`) values('42','橘子','20','80','非常不错');
+insert into `t_fruit` (`fid`, `fname`, `price`, `fcount`, `remark`) values('43','柚子','23','11','美滴很');
+insert into `t_fruit` (`fid`, `fname`, `price`, `fcount`, `remark`) values('44','山李子','10','88','物美价廉');
+insert into `t_fruit` (`fid`, `fname`, `price`, `fcount`, `remark`) values('63','木瓜','23','80','美滴很');
+insert into `t_fruit` (`fid`, `fname`, `price`, `fcount`, `remark`) values('66','杨桃','19','20','是杨桃哦');
+insert into `t_fruit` (`fid`, `fname`, `price`, `fcount`, `remark`) values('69','吗啡','20','10','好吃');
+insert into `t_fruit` (`fid`, `fname`, `price`, `fcount`, `remark`) values('70','柠檬','20','10','好吃');
+insert into `t_fruit` (`fid`, `fname`, `price`, `fcount`, `remark`) values('72','番茄','23','80','非常不错');
